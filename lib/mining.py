f962291db8110bad630a9e110b115f2ab52f31ed
Piotr Mrowczynski <mrow4a@yahoo.com>
2017-10-31 13:46:02 +0100

    Add pagination

--- lib/mining.py
+++ lib/mining.py
@@ -2,6 +2,7 @@
 
 from github import Github
 import os, json
+import time
 
 class Miner:
 
@@ -13,6 +14,20 @@ class Miner:
         self.logfile = os.path.dirname(os.path.realpath(__file__)) + "/results.log"
         self.auth = Github(user, password)
 
+    def get_results(self, paged_results):
+        file = open(self.logfile, 'w')
+        for i in range(0,paged_results.totalCount):
+            page = paged_results.get_page(i)
+            for res in page:
+                json_line = json.dumps({"name": res.repository.full_name, "id": res.repository.id})
+                print json_line
+                file.write(json_line + '\n')
+            #print pages
+            time.sleep(2)
+        file.close()
+
+        print "WROTE URLS TO %s"%(self.logfile)
+
     def get_repos_for_keyword(self, keyword, language):
         if (language not in [self.JAVA, self.SCALA, self.PYTHON]):
             print("WRONG LANGUAGE - Please use Miner.JAVA | Miner.SCALA | Miner.PYTHON")
@@ -20,18 +35,5 @@ class Miner:
         print('Mining ' + keyword + '...')
         print
 
-        #For now, lets just write to file, later we can play with appending
-        #f = open(self.logfile, 'a+')
-        file = open(self.logfile, 'w')
-
-        #Loop over all found repositories with the code
-        counter = 0
-        for repo in self.auth.search_code(keyword):
-            if (counter > 100):
-                break
-            counter = counter + 1
-            json_line = json.dumps({ "url" : repo.url })
-            file.write(json_line + '\n')
-
-        print "WROTE %s URLS TO %s"%(counter, self.logfile)
-        file.close()
+        paged_results = self.auth.search_code(keyword)
+        self.get_results(paged_results)
