5b7809769b540f85bd34ad039fd1c17bb058a207
Matyas Manninger <manninger.matyas@gmail.com>
2017-11-16 12:32:21 +0100

    add aggregating script

---
+++ lib/aggregate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import json
+
+data = dict()
+with open('gelly.log') as f:
+    for line in f:
+        j = json.loads(line)
+        data[j['id']] = j
+
+open_issues = sum(map(lambda j: j['open_issues_count'], data.values()))
+subscribers = sum(map(lambda j: j['subscribers_count'], data.values()))
+watchers = sum(map(lambda j: j['watchers_count'], data.values()))
+stars = sum(map(lambda j: j['stargazers_count'], data.values()))
+
+print("open_issues: " + str(open_issues) + ", per repo: " + str(open_issues / float(len(data))))
+print("subscribers: " + str(subscribers) + ", per repo: " + str(subscribers / float(len(data))))
+print("watchers: " + str(watchers) + ", per repo: " + str(watchers / float(len(data))))
+print("stars: " + str(stars) + ", per repo: " + str(stars / float(len(data))))
